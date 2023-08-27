b201de0c04d1a40981d452832fc4407bc93af8b9
DILIP SANAP <dilipinbsanap@gmail.com>
2023-08-27 22:05:19 +0530

    register user route added

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,5 +1,8 @@
 const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
+// get all users
 exports.getAllUsers = async(req,res) => {
     try {
         const users = await User.find();
@@ -10,5 +13,30 @@ exports.getAllUsers = async(req,res) => {
             message:
             "Something went wrong at getting all users"
         })
+    }
+}
+
+// register the user
+exports.registerUser = async(req,res) => {
+    try {
+        const { username, email, password } = req.body;
+        const getUser = await User.findOne({ email });
+        if (getUser) {
+            return res.status(409).json({message:"User with this email is allready exist"})
+        }
+        bcrypt.hash(password, 10, async(err, hash) => {
+            if (err) {
+                return res.status(404).json({message:"Something went wrong at hashing the password"})
+            } else {
+                const user = new User({ username, email, password:hash });
+                await user.save();
+                res.status(201).json({message:"User register successfully",user})
+            }
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            message: "Something went wrong at register the user",
+        });
     }
 }
