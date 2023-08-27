72044a3ada51bb8752f9bfbcf76862b9978b7319
DILIP SANAP <dilipinbsanap@gmail.com>
2023-08-27 21:28:58 +0530

    Connected to Database

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const connection=require('./config/db')
 const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).json({message:"Welcome to MentorStudents app"})
 })
 
-app.listen(7000, () => {
+app.listen(7000, async () => {
+    try {
+        await connection;
+        console.log('Connected to Db');
+    } catch (error) {
+        console.log('Error at conncting DB:',error);
+    }
     console.log('server is sunning on port:7000');
 });
